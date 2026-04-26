7f89e37ee9b727b1628992536a1cd78ffa27a7d9
凡哲 CH <CHfanzhe@outlook.com>
2026-04-26 19:06:50 +0800

    Update

--- StrongType/Src/main.cpp
+++ StrongType/Src/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <StrongType/StrongType.hpp>
 #include <mutex>
+int add(int a, int b) noexcept { return a + b; }
 int main() {
 	StrongType::wrapper<int, struct MyTag, StrongType::inherit::non_inherit, StrongType::modifier::arithmetic> myWrapper = 1;
 	std::cout << myWrapper.value() << std::endl;
-	std::cout << (myWrapper += 100).value() << std::endl;;
+	std::cout << (myWrapper += 100).value() << std::endl;
 	std::cout << myWrapper.value() << std::endl;
 }
--- StrongType/include/StrongType/StrongType.hpp
+++ StrongType/include/StrongType/StrongType.hpp
@@ -3,4 +3,3 @@
 #include "wrapper.hpp"
 #include "inherit.hpp"
 #include "modifier.hpp"
-#include "modifier_convert.hpp"
